e1bc145157c82d62fd448d469f5efe9f841f4bef
Gavin Schramm <gavin.schramm@doane.edu>
2023-10-13 21:17:07 -0500

    Exercise 4  Acceptable Level

--- Module 4/Exercise 4.js
+++ Module 4/Exercise 4.js
@@ -3,8 +3,9 @@ async function getBaconipsum() {
     var apiString = "https://baconipsum.com/api/";
     // next add the parameters to the string using the drop down lists
     var theNewParagraphs = document.getElementById("newParagraphs").value;
-    apiString = apiString + "?type=meat-and-filler&paras=" + theNewParagraphs;
-    alert(apiString); //show the API string
+    // asks the user for either all meat or meat and filler
+    var allMeat = document.getElementById("allMeat").value;
+    apiString = apiString + "?type=" + allMeat + "&paras=" + theNewParagraphs;
 
     // now make the API call to the web service using the string and store what is returned in response
     var response = await fetch(apiString);
@@ -23,5 +24,45 @@ async function getBaconipsum() {
         document.getElementById("myFormattedData").innerHTML += "<p>" + jsonData[para] + "</p>";
     }
 
+    caesar();
+
     return true;
+}
+
+// Function for encription
+function caesar() {
+    var encr = document.getElementById("encr").value;
+    var plainText = document.getElementById("myFormattedData").innerHTML;
+    var encrText = "";
+    var alpha = "abcdefghijklmnopqrstuvwxyz .";
+    var newText = "";
+    
+    // Checks if the user wants it encrypted
+    if (encr == "no") {
+        document.getElementById("spicyText").innerHTML = plainText;
+    // if yes, do the encryption
+    } else if (encr == "yes") {
+        var lowerText = plainText.toLowerCase(); // set the string to lowercase
+    
+        // check each letter in the plain text to see if its in the alphabet and adds it to a new string
+        for (var i in lowerText){
+            for (var j in alpha){
+                if(lowerText[i].includes(alpha[j])) {
+                newText += lowerText[i];
+                }
+            }
+        }
+
+        // takes the new string and does the ceaser encryption
+        for (var ch in newText) {
+            var newLetter = newText.charCodeAt(ch) + 1;
+            if (newLetter - 1 == 32){ // checks to see if the character is a space
+                encrText += " ";
+            } else {
+            var encrLetter = String.fromCharCode(newLetter);
+            encrText += encrLetter;
+            }
+        }
+        document.getElementById("spicyText").innerHTML = encrText;
+    }
 }
